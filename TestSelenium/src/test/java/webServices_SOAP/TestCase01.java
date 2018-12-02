b35fbba7e720b1269a1f9bd2f7661f767d9eb2c2
sandeeplunawath <sapphirej16@gmail.com>
2018-12-02 13:33:47 +0530

    updated soap method with basic script

--- TestSelenium/src/test/java/webServices_SOAP/TestCase01.java
+++ TestSelenium/src/test/java/webServices_SOAP/TestCase01.java
@@ -7,16 +7,16 @@ import org.apache.commons.io.FileUtils;
 import org.testng.annotations.Test;
 
 import junit.framework.Assert;
-import utils.ExcelInteractions;
-import utils.ReadExcel;
-import utils.SoapUIInteraction;
+/*import utils.ExcelInteractions;
+import utils.ReadExcel;*/
+import webServices_SOAP.SoapUIInteraction;
 
 public class TestCase01 {
 
 	@Test
 	public void ExecuteReasons() throws IOException
 	{
-		String soapURL = "https://ctstsoa.stage.aa.com/ResolutionDiscoveryWeb/sca/ResolutionDiscoveryServiceV2";
+		/*String soapURL = "https://ctstsoa.stage.aa.com/ResolutionDiscoveryWeb/sca/ResolutionDiscoveryServiceV2";
 		ExcelInteractions excel = new ExcelInteractions();
 		String excelData[][]= excel.getAllDataRowsFromSheet("CompensationMatrix","Reasons");
 		PrintWriter writer = new PrintWriter(System.getProperty("user.dir") +"/Log/ExecuteReasons.txt", "UTF-8");		
@@ -68,6 +68,20 @@ public class TestCase01 {
 			writer.println("Fail");
 			writer.close();
 			Assert.fail();
-		}
+		}*/
 	}
+
+	@Test
+	public void basicScript() throws IOException
+	{
+		String soapURL = "https://ctstsoa.stage.aa.com/ResolutionDiscoveryWeb/sca/ResolutionDiscoveryServiceV2";
+		String filePath =  System.getProperty("user.dir") +"/Files/GetSubCategories.xml";			
+		String soapXml = FileUtils.readFileToString(new File(filePath));
+		
+		soapXml= soapXml.replace("CLAIMTYPEDETAILS", "")
+				.replace("&", "&amp;");
+		SoapUIInteraction	objprocess = new SoapUIInteraction();
+		String responseData =objprocess.SoapUIProcessing(soapURL, soapXml);
+	}
+
 }
